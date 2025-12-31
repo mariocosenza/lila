b50217a61fad82e271fb34c7aecb15c71dc0bebd
Mario Cosenza <61911701+mariocosenza@users.noreply.github.com>
2025-12-31 01:07:08 +0100

    feat: add mcp tools

--- mcp/server.py
+++ mcp/server.py
@@ -2,33 +2,40 @@ import json
 from typing import Annotated
 from fastmcp import FastMCP
 
+from grammo.src.grammo.service import compile_text, run_tests
 from syntax_lark import GrammoParser
+
 mcp = FastMCP("grammo-mcp", strict_input_validation=True)
 
 parser_instance = GrammoParser()
+
 @mcp.tool(
-      name="grammo_lark",
-      description="Check the correctness of Grammo syntax requires the code as input",
-      enabled=True
+    name="grammo_lark",
+    description="Check the correctness of Grammo syntax requires the code as input",
+    enabled=True,
 )
 def syntax_checker(code: Annotated[str, "The generated Grammo Code"]) -> dict:
-   result = parser_instance.validate(code)
-   return result
+    result = parser_instance.validate(code)
+    return result
 
 @mcp.tool(
-      name="grammo_compiler",
-      description="Compile the code and return the compiled status",
-      enabled=True
+    name="grammo_compiler",
+    description="Compile the code and return the compiled status",
+    enabled=True,
 )
 def compiler(code: Annotated[str, "The generated Grammo Code"]) -> dict:
-   return {
-      "compiled": True,
-      "info": "",
-      "warning": "",
-      "errors": ""
-   }
+    return compile_text(code, opt_level=3)
 
-
+@mcp.tool(
+    name="grammo_test",
+    description="Test the code and return result of the tests",
+    enabled=True,
+)
+def tester(
+    code: Annotated[str, "The generated Grammo Code"],
+    tests: Annotated[str, "The test in Grammo Code"],
+) -> dict:
+    return run_tests(code, tests)
 
 if __name__ == "__main__":
-   mcp.run(transport="http", host="0.0.0.0", port=8000)
+    mcp.run(transport="http", host="0.0.0.0", port=8000)
